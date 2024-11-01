79c616ced4cf4e1c3f358e76eed7e4b8f3bd8e37
Avinash <avinashselvan11@gmail.com>
2024-11-01 12:46:57 +0530

    Added pilots views urls templates

---
+++ pilots/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name='pilots' # Namespacing
+
+urlpatterns = [
+    path('', views.pilot_list, name ='pilot_list'),
+    path('<int:pilot_id>/', views.pilot_detail, name = "pilot_detail")
+]
--- pilots/views.py
+++ pilots/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Pilot
 
 # Create your views here.
+def pilot_list(request):
+    pilots = Pilot.objects.all()
+    return render(request, 'pilots/pilot_list.html', {'pilots': pilots})
+
+def pilot_detail(request, pilot_id):
+    pilot = get_object_or_404(Pilot, pk=pilot_id)
+    return render(request, 'pilots/pilot_detail.html', {'pilot': pilot})
