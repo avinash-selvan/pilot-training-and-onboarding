1339d34d3ce8c685d6a5708bb416f5402799f18e
Avinash <avinashselvan11@gmail.com>
2024-11-03 21:54:19 +0530

    inital commit for procedures

--- enrollment/urls.py
+++ enrollment/urls.py
@@ -6,4 +6,6 @@ app_name = 'enrollment'
 urlpatterns = [
     path('', views.enrollment_list, name='enrollment_list'),
     path('<int:enrollment_id>/', views.enrollment_detail, name='enrollment_detail'),
+    path('enrolled_pilots/<int:session_id>/', views.list_enrolled_pilots_by_session, name='list_enrolled_pilots_by_session'),
 ]
+
--- enrollment/views.py
+++ enrollment/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Enrollment
+from django.db import connection
 
 def enrollment_list(request):
     enrollments = Enrollment.objects.all()
@@ -8,3 +9,14 @@ def enrollment_list(request):
 def enrollment_detail(request, enrollment_id):
     enrollment = get_object_or_404(Enrollment, enrollment_id=enrollment_id)
     return render(request, 'enrollment/enrollment_detail.html', {'enrollment': enrollment})
+
+def list_enrolled_pilots_by_session(request, session_id):
+    with connection.cursor() as cursor:
+        cursor.callproc("ListEnrolledPilotsBySession", [session_id])
+        results = cursor.fetchall()
+    
+    context = {
+        'results': results,
+        'session_id': session_id
+    }
+    return render(request, 'enrollment/list_enrolled_pilots.html', context)
