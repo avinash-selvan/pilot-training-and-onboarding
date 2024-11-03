40711afd31856bb2171bc7f7e85b773eeb5d9e8b
Avinash <avinashselvan11@gmail.com>
2024-11-03 19:22:22 +0530

    Initial commit

--- pilots/urls.py
+++ pilots/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.pilot_list, name ='pilot_list'),
     path('<int:pilot_id>/', views.pilot_detail, name = "pilot_detail"),
     path('add/', views.add_pilot, name='add_pilot'),
+    path('edit/<int:pilot_id>/', views.edit_pilot, name='edit_pilot'),
 ]
--- pilots/views.py
+++ pilots/views.py
@@ -20,3 +20,17 @@ def add_pilot(request):
     else:
         form = PilotForm()
     return render(request, 'pilots/add_pilot.html', {'form': form})
+
+def edit_pilot(request, pilot_id):
+    pilot = get_object_or_404(Pilot, pk=pilot_id)
+    
+    if request.method == "POST":
+        form = PilotForm(request.POST, instance=pilot)
+        if form.is_valid():
+            form.save()
+            # Redirect to pilot list or detail page after saving
+            return redirect('pilots:pilot_detail', pilot_id=pilot_id)
+    else:
+        form = PilotForm(instance=pilot)
+
+    return render(request, 'pilots/edit_pilot.html', {'form': form, 'pilot': pilot})
